b13d63177a4f170d0928bdcebee4893809011ff1
Cinco Padilla Ramon Eduardo - 302 <torbellinokien710@gmail.com>
2025-12-03 06:19:46 -0600

    Initial commit: VentifyAPI - Sistema POS multi-tenant con ventas, caja, descuentos y reportes

---
+++ Models/RefreshToken.cs
@@ -0,0 +1,25 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VentifyAPI.Models
+{
+    public class RefreshToken
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Token { get; set; } = null!;
+
+        [Required]
+        public int UsuarioId { get; set; }
+
+        public DateTime ExpiresAt { get; set; }
+
+        public bool Revoked { get; set; }
+
+        [ForeignKey(nameof(UsuarioId))]
+        public Usuario? Usuario { get; set; }
+    }
+}
